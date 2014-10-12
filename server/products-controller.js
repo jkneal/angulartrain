4d35ef25dfe861f3256221939c1ec7976b8167c6
jkneal <jkneal@indiana.edu>
2014-10-12 18:56:26 -0400

    Add products controller and dependency on body-parser

---
+++ server/products-controller.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var products = [
+    {
+        name: "Basketball", description: "IU Branded",
+        category: "Game Balls", price: 20
+    },
+    {
+        name: "Football", description: "Purdue Branded",
+        category: "Game Balls", price: 35
+    }
+];
+
+module.exports = function(app) {
+    app.use(bodyParser());
+
+    app.get('/products', function(req, res) {
+        console.log('getting all products on server');
+        res.json(products);
+    });
+
+    app.post('/products', function(req, res) {
+        console.log('add new product on server: ' + req.body);
+        products.push(req.body);
+        res.json(products);
+    });
+
+    app.post('/products/remove', function(req, res) {
+        console.log('deleting product with index: ' + req.body.index);
+        products.splice(req.body.index, 1);
+        res.json(products);
+    });
+};
