08163793964dc792f7bc69616de3568b716e0ab9
jkneal <jkneal@indiana.edu>
2014-10-12 12:11:15 -0400

    solution for exercise 3

--- client/scripts/controllers/store-controller.js
+++ client/scripts/controllers/store-controller.js
@@ -1,8 +1,37 @@
 angular.module('storeApp').controller('storeCrtl', function ($scope) {
     $scope.name = 'Joe\'s Sports Store';
     $scope.address = '5100 51st Chicago, IL';
+    $scope.status = 'Open';
 
-    $scope.getStatus = function() {
-        return 'Open';
+    $scope.getStatus = function () {
+        return $scope.status;
+    }
+
+    $scope.products = [
+        {
+            name: "Basketball", description: "IU Branded",
+            category: "Game Balls", price: 20
+        },
+        {
+            name: "Football", description: "Purdue Branded",
+            category: "Game Balls", price: 35
+        }
+    ];
+
+    $scope.addProduct = function (productName, productDescription, productCategory, productPrice) {
+        $scope.products.push({
+            name: productName,
+            description: productDescription,
+            category: productCategory,
+            price: productPrice
+        });
+
+        $scope.productAdded = true;
+    }
+
+    $scope.productAdded = false;
+
+    $scope.removeProduct = function (index) {
+        $scope.products.splice(index, 1);
     }
 });
