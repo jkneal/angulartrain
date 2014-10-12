d08ee7d5d83c6857ed68540118f191009cb4dae0
jkneal <jkneal@indiana.edu>
2014-10-12 16:21:57 -0400

    solution for exercise 4

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,19 @@
 angular.module('storeApp', []);
+
+angular.module('storeApp').filter('categories', function () {
+        return function (items) {
+            var categories = [];
+            var keys = {};
+
+            for (var i = 0; i < items.length; i++) {
+                var val = items[i].category;
+                if (angular.isUndefined(keys[val])) {
+                    keys[val] = true;
+                    categories.push(val);
+                }
+            }
+
+            return categories;
+        };
+    }
+);
